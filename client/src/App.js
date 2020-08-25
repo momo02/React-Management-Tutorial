a4aabae242e69e5a6029c8439c9990a8825cf5da
momo02 <jungha0902@gmail.com>
2020-08-25 17:49:37 +0900

    고객 목록 조회 REST API 구현 및 연동

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 //components 폴더에 있는 Customer 컴포넌트를 불러와서 App 컴포넌트안에서 출력
 import Customer from "./components/Customer";
@@ -23,68 +23,60 @@ const styles = (theme) => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "박모모",
-    birthday: "930909",
-    gender: "여자",
-    job: "개발자",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "김모모",
-    birthday: "930909",
-    gender: "남자",
-    job: "디자이너",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "이모모",
-    birthday: "930909",
-    gender: "여자",
-    job: "퍼블리셔",
-  },
-];
+class App extends Component {
+  state = {
+    customers: "",
+  };
 
-function App(props) {
-  const { classes } = props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers.map((c) => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
+
+  callApi = async () => {
+    //자기자신, 로컬호스트의 /api/customers 호출. package.json에 프록시 서버 설정을 해두었으니 http://localhost:5000/api/customers 가 호출될 것.
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers
+              ? this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              : ""}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 export default withStyles(styles)(App);
-
-//cf) meterial-ui table component demo Site : https://material-ui.com/components/tables/
--- server.js
+++ server.js
@@ -4,12 +4,37 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000; //서버 포트 5000번으로 설정
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //json 형식으로 데이터를 주고받는다.
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//api 명시. "/api/hello" 호출 시, 클라이언트에게 "Hello Express!"라는 메시지를 리턴
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+//고객 목록 조회 api 명시
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "박모모",
+      birthday: "930909",
+      gender: "여자",
+      job: "개발자",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "김모모",
+      birthday: "930909",
+      gender: "남자",
+      job: "디자이너",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "이모모",
+      birthday: "930909",
+      gender: "여자",
+      job: "퍼블리셔",
+    },
+  ]);
 });
 
 // 5000번 포트로 앱을 동작 시키고, 동작시 로그 출력.
